d2b7d677439a8f027de62495de43e577bfcd60cd
Ezeco07 <moreirafranco79@gmail.com>
2023-08-07 21:28:23 -0300

    Summary
    Cambios

--- js/script.js
+++ js/script.js
@@ -1,23 +1,20 @@
-// En este archivo no utilizamos el evento "DOMContentLoaded", ya que se colocó el atributo "defer" en la importación del script,
-// que nos soluciona el problema de los elementos no cargados del DOM. Más info => https://www.w3schools.com/tags/att_script_defer.asp
+const DATA_URL = "json/data.json"; 
 
-const DATA_URL = "json/data.json"; // URL que contiene los datos que queremos mostrar
+const container = document.getElementById("container"); 
 
-const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
-
-/**
- * Función que recibe por parámetro un array con los datos que se mostrarán en el DOM
- * Los datos se mostrarán dentro del div de id "container" y por cada ítem se está creando un nuevo párrafo donde se
- * imprime el campo "name" y el campo "lastname" separados por un espacio
- */
 function showData(dataArray) {
-  // El for itera sobre los elementos del array
-  for (const item of dataArray) {
-    // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
-    container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
+  container.innerHTML = '';
+
+  
+  for (const item of dataArray){
+   
+    const paragraph = document.createElement('p'); 
+    paragraph.textContent = `${item.name} ${item.lastname}`; 
+    container.appendChild(paragraph); 
   }
 }
 
+<<<<<<< Updated upstream
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
 
 async function traerDatos(url) {
@@ -32,3 +29,8 @@ async function traerDatos(url) {
 }
 
 traerDatos(DATA_URL);
+=======
+fetch(DATA_URL)
+  .then(response => response.json())
+  .then(dataArray => showData(dataArray.students)); 
+>>>>>>> Stashed changes
